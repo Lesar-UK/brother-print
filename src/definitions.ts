9b532161e8785d8992b5f210da575d11a09a644b
Kieran Barlow <kieran.barlow@lesar.co.uk>
2024-11-04 17:09:32 +0000

    add missing definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -2,5 +2,6 @@ export interface BrotherPrintPlugin {
   searchWifiPrinters(): Promise<{ printers: string[] }>;
   searchBluetoothPrinters(): Promise<{ printers: string[] }>;
   base64Print(): Promise<{ message: string }>;
+  base64PDFPrint(): Promise<{ message: string }>;
   checkPrinterStatus(): Promise<{ status: string[] }>;
 }
